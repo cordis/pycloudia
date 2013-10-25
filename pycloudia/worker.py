43434084cf4d434a6ad8b3ab04b264510e531f9e
cordis <cordis@game-mafia.ru>
2013-10-25 21:28:45 +0800

    + local host discovery by interface

--- pycloudia/worker.py
+++ pycloudia/worker.py
@@ -1,21 +1,21 @@
-from collections import namedtuple
 from pycloudia.uitls.defer import inline_callbacks
-
-
-WorkerOptions = namedtuple('WorkerOptions', 'zmq udp interface cluster groups')
+from pycloudia.uitls.net import get_ip_address
 
 
 class Worker(object):
     reactor = None
     console = None
 
-    def __init__(self, options):
-        self.options = options
-
-    def initialize(self):
-        self.reactor.register_for_shutdown(self._shutdown)
+    def __init__(self, port, interface='eth0', group=None, roles=None):
+        self.port = port
+        self.host = get_ip_address(interface)
+        self.group = group
+        self.roles = roles or []
+#        self.cluster = Cluster(options)
+#        self.threads = Threads()
 
     def run(self):
+        self.reactor.register_for_shutdown(self._shutdown)
         self.reactor.call_when_running(self._run)
         self.reactor.run()
 
