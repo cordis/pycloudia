85ab0689b1e0b59aa0a2bb8f546589980ab610d6
cordis <cordis@game-mafia.ru>
2013-10-25 21:47:30 +0800

    + experimental sphinx docstring

--- pycloudia/uitls/net.py
+++ pycloudia/uitls/net.py
@@ -3,10 +3,16 @@ import fcntl
 import struct
 
 
+SIOCGIFADDR = 0x8915
+
+
 def get_ip_address(interface_name):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', interface_name[:15])
-    )[20:24])
+    """
+    :param interface_name: str
+    :rtype: str
+    :raise: IOError
+    """
+    connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    packed_interface = struct.pack('256s', interface_name[:15])
+    packed_ip = fcntl.ioctl(connection.fileno(), SIOCGIFADDR, packed_interface)[20:24]
+    return socket.inet_ntoa(packed_ip)
