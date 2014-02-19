efe20de644edcd4f926729679de62336b5b17476
cordis <cordis@game-mafia.ru>
2014-02-19 22:55:14 +0800

    f request param getter improved to accept expected value type

--- pycloudia/rest/interfaces.py
+++ pycloudia/rest/interfaces.py
@@ -7,23 +7,14 @@ class IRequest(object):
     MISSED = object()
 
     @abstractmethod
-    def get_argument(self, name, default=MISSED):
+    def get_argument(self, type_func, name, default=MISSED):
         """
+        :type type_func: C{Callable} or C{None}
         :type name: C{str}
-        :type default: C{str} or C{None}
-        :rtype: C{str}
+        :type default: C{object} or C{None}
+        :rtype: C{type_func}
         :raise: L{pycloudia.rest.exceptions.MissingArgumentError}
         """
-
-    @abstractmethod
-    def get_argument_as_list(self, name, default=MISSED):
-        """
-        :type name: C{str}
-        :type default: C{list}
-        :rtype: C{list} of C{str}
-        :raise: L{pycloudia.rest.exceptions.MissingArgumentError}
-        """
-
 
     @abstractproperty
     def path(self):
